c8052a4bac65f671abe18282ae78090075e719b8
Jessica <jlswans1@gmail.com>
2019-06-08 18:04:51 -0700

    Build initial styles and welcome component

---
+++ src/components/Welcome.vue
@@ -0,0 +1,59 @@
+<template>
+    <b-container>
+        <b-row>
+            <b-col class="text-center">
+                <img src="../assets/logo.png" alt="Brooklyn Logo" title="Welcome to the Brooklyn application!">
+                <h1>Welcome!</h1>
+            </b-col>
+        </b-row>
+        <b-row>
+            <b-col class="text-center">
+                <p>Who's playing?</p>
+                <b-form>
+                    <b-form-group>
+                        <b-form-input v-model="form.name">
+                        </b-form-input>
+                    </b-form-group>
+                    
+                    <b-button size="sm" variant="secondary">Let's go!</b-button>
+                </b-form>
+            </b-col>
+        </b-row>
+    </b-container>    
+</template>
+
+<script>
+export default {
+    name: 'Welcome',
+    data() {
+        return {
+            form: {
+                name: '',
+            }
+        }
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+    @import '../styles/custom.scss';
+
+    .container {
+        color: $white;
+    }
+
+    img {
+        max-width: 10em;
+    }
+
+    h1 {
+        font-size: 3.5rem;
+        font-weight: 700;
+    }
+
+    p {
+        font-size: 1.5rem;
+    }
+</style>
+
+
